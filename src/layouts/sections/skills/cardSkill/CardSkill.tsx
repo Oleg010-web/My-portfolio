086284b666ab86ecf91dd2a6763f87b90dd8e713
Oleg010-web <nesteroff_oleg@mail.ru>
2024-04-05 19:48:22 +0300

    Add-styles-to-skills-section-working-with-pseudo-elements

--- src/layouts/sections/skills/cardSkill/CardSkill.tsx
+++ src/layouts/sections/skills/cardSkill/CardSkill.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import { Icon } from "../../../../components/icon/Icon"
+import { FlexWrapper } from "../../../../components/FlexWrapper"
 
 
 type CardSkillPropsType = {
@@ -9,28 +10,55 @@ type CardSkillPropsType = {
 
 }
 
-export const CardSkill = (props : CardSkillPropsType) => {
+export const CardSkill = (props: CardSkillPropsType) => {
   return (
     <CardSkillStyled>
-      <Icon iconId={props.iconId} />
-      <SkillTitleStyled>{props.title}</SkillTitleStyled>
-      <SkillTextStyled>{props.desription}</SkillTextStyled>
+      <FlexWrapper direction="column" align="center">
+        <IconWrapper>
+          <Icon iconId={props.iconId} />
+        </IconWrapper>
+        <SkillTitleStyled>{props.title}</SkillTitleStyled>
+        <SkillTextStyled>{props.desription}</SkillTextStyled>
+      </FlexWrapper>
     </CardSkillStyled>
   )
 }
 
 
 const CardSkillStyled = styled.div`
-  width: 30%;
-  color: #424def;
-  background-color: #b29bf7;
-  margin: 10px;
+  max-width: 380px;
+  padding: 62px 20px 40px;
 `
 
 const SkillTitleStyled = styled.h3`
-  
+  margin: 70px 0 15px;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 700;
+  font-size: 16px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
 `
 
 const SkillTextStyled = styled.p`
-  
+  text-align: center;
+  font-weight: 400;
+  font-size: 14px;
+`
+
+const IconWrapper = styled.div`
+  position: relative;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    background: rgba(255,255,255,0.1);
+    transform: rotate(45deg) translate(-50%, -50%);
+
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform-origin: top left;
+  }
 `
