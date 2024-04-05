4125e2d29b37b04b8b77d37d6bfc5ffb2ef3afa1
Oleg010-web <nesteroff_oleg@mail.ru>
2024-04-05 11:10:56 +0300

    Add-styles-to-main-section-working-with-pseudo-elements

--- src/layouts/sections/main/Main.tsx
+++ src/layouts/sections/main/Main.tsx
@@ -1,18 +1,24 @@
 import styled from 'styled-components'
 import myPhoto from '../../../assets/images/myPhoto.jpg'
 import { FlexWrapper } from '../../../components/FlexWrapper'
+import { Container } from '../../../components/Container'
+import { theme } from '../../../styles/Theme'
 
 export const Main = () => {
   return (
     <StyledMain>
-      <FlexWrapper align={"center"} justify={"space-around"}>
-        <div>
-          <span>Hi There</span>
-          <StyledName>I am Oleg Nesterov</StyledName>
-          <StyledMainTitle>A Web Developer</StyledMainTitle>
-        </div>
-        <StyledPhoto src={myPhoto} alt='' />
-      </FlexWrapper>
+      <Container>
+        <FlexWrapper align={"center"} justify={"space-around"}>
+          <div>
+            <Greetings>Hi There</Greetings>
+            <StyledName>I am <span>Oleg Nesterov</span></StyledName>
+            <StyledMainTitle>A Web Developer</StyledMainTitle>
+          </div>
+          <PhotoStub>
+            <StyledPhoto src={myPhoto} alt='' />
+          </PhotoStub>
+        </FlexWrapper>
+      </Container>
     </StyledMain>
   )
 }
@@ -21,6 +27,23 @@ export const Main = () => {
 const StyledMain = styled.section`
   min-height: 100vh;
   background-color: #c82d2d;
+  display: flex;
+`
+const PhotoStub = styled.div`
+  position: relative;
+  z-index: 1;
+
+  &::before{
+    content: '';
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    z-index: -1;
+  }
 `
 
 const StyledPhoto = styled.img`
@@ -29,9 +52,38 @@ const StyledPhoto = styled.img`
   object-fit: cover;
 `
 const StyledMainTitle = styled.h1`
-  
+  font-weight: 400;
+  font-size: 27px;
+  line-height: 40px;
 `
 
-const StyledName = styled.span`
-  
+const Greetings = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 21px;
+`
+
+const StyledName = styled.h2`
+  font-family: 'Josefin Sans';
+  font-weight: 700;
+  font-size: 50px;
+  line-height: 50px;
+  margin: 10px 0;
+
+  span {
+    position: relative;
+    z-index: 1;
+
+    &::before {
+      content: '';
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+    }
+  }
 `
