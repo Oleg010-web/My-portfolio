6e9b42521e8ad200a928eb2eaac953c9b64bd303
Oleg010-web <nesteroff_oleg@mail.ru>
2024-04-09 23:48:12 +0300

    Add-styles-to-footer-section

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import { Icon } from "../components/icon/Icon"
 import { FlexWrapper } from "../components/FlexWrapper"
+import { theme } from "../styles/Theme"
 
 export const Footer = () => {
   return (
@@ -36,18 +37,23 @@ export const Footer = () => {
 }
 
 const FooterStyled = styled.footer`
-  min-height: 50vh;
-  background-color: #a3d3ff;
+  background-color: ${theme.colors.primaryBg};
+  padding: 40px 0;
 `
 
 const StyledName = styled.span`
-  
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 700;
+  font-size: 22px;
+  letter-spacing: 3px;
 `
 
 const StyledSocialLinks = styled.ul`
   display: flex;
   list-style: none;
-  gap: 30px;
+  gap: 20px;
+  margin: 30px 0;
+
 `
 
 const SocialLinkItem = styled.li`
@@ -55,9 +61,25 @@ const SocialLinkItem = styled.li`
 `
 
 const StyledSocialLink = styled.a`
+  background-color: rgba(255,255,255,0.1);
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+  color: ${theme.colors.accent};
+
+  &:hover {
+    color: ${theme.colors.primaryBg};
+    transform: translateY(-4px);
+  }
 `
 
 const StyledCopyright = styled.small`
-  
+  font-weight: 400;
+  font-size: 12px;
+  text-align: center;
+  opacity: 0.5;
 `
--- src/layouts/sections/slogan/Slogan.tsx
+++ src/layouts/sections/slogan/Slogan.tsx
@@ -1,12 +1,18 @@
 import styled from "styled-components"
 import { SectionTitle } from "../../../components/SectionTitle"
 import { Button } from "../../../components/Button"
+import { Container } from "../../../components/Container"
+import { FlexWrapper } from "../../../components/FlexWrapper"
 
 export const Slogan = () => {
   return (
     <StyledSlogan>
-      <SectionTitle>Let's make code together, man!</SectionTitle>
-      <Button>Hire me</Button>
+      <Container>
+        <FlexWrapper direction="column" align="center">
+          <SectionTitle>Let's make code together, man!</SectionTitle>
+          <Button>Hire me</Button>
+        </FlexWrapper>
+      </Container>
     </StyledSlogan>
   )
 }
